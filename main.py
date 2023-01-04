dbf0629141b02568282bb7d7f129f9b12afb62dc
zjzj1996 <guaguaguaxia@gmail.com>
2023-01-04 15:05:08 +0800

    feat:测试名称

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("我刚才提交了什么?")
+    print("测试名称")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
